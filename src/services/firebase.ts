893bdbb54a961aeb84e918a05bdd8154c7a030d4
heroual <mekocaw182@operades.com>
2024-11-27 18:47:41 +0100

    32215

---
+++ src/services/firebase.ts
@@ -0,0 +1,9 @@
+// Re-export all Firebase services
+export * from './firebase/auth';
+export * from './firebase/config';
+export * from './firebase/collections';
+export * from './firebase/tickets';
+export * from './firebase/devices';
+export * from './firebase/email';
+export * from './firebase/action-plans';
+export * from './firebase/feedback';
